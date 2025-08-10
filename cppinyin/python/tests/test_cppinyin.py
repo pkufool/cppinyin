91b36b1e4a1866d7272ce344c9c6df73797a54fd
pkufool <wkang.pku@gmail.com>
2025-08-10 16:50:11 +0800

    fix test

--- cppinyin/python/tests/test_cppinyin.py
+++ cppinyin/python/tests/test_cppinyin.py
@@ -33,7 +33,7 @@ class TestEncode(unittest.TestCase):
         pinyins = [
             "yi1 qie4 fan3 dong4 pai4 dou1 shi4 zhi3 lao3 hu3",
             "yi2 jiang1 sheng4 yong3 zhui1 qiong2 kou4 bu4 ke3 gu1 ming2 xue2 ba4 wang2",
-            "wo3 shi4 zhong1 guo2 ren2 min2 de er2 zi5",
+            "wo3 shi4 zhong1 guo2 ren2 min2 de er2 zi",
         ]
         for i, p in enumerate(phrases):
             res, seg = cpp.encode(p, return_seg=True)
